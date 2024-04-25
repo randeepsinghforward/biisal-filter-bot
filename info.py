6811147113fd76859ff6b0397a915333d4988a40
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-04-25 18:58:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tejnarayankumar3546:OxqIiYyR68rEKmrL@biisalbotmongo.brs3egg.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://immortal:5412ascs@immortal.jehlw9n.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "biisalbotmongo")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
